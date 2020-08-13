cfc58a5bde2ccfe880c126d59d308ee2a548d934
Gabriel Lorandi <glorandi.oficial@gmail.com>
2020-08-12 21:53:52 -0300

    tela inicial de voto

---
+++ src/Voto/VotoController.java
@@ -0,0 +1,60 @@
+package Voto;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class VotoController {
+
+    @FXML Button btn1;
+    @FXML Button btn2;
+    @FXML Button btn3;
+    @FXML Button btn4;
+    @FXML Button btn5;
+    @FXML Button btn6;
+    @FXML Button btn7;
+    @FXML Button btn8;
+    @FXML Button btn9;
+
+    @FXML Button btnBranco;
+    @FXML Button btnConfirmar;
+    @FXML Label numeroCandidato;
+
+
+    public void digito1(ActionEvent actionEvent) {
+    }
+
+    public void digito2(ActionEvent actionEvent) {
+    }
+
+    public void digito3(ActionEvent actionEvent) {
+    }
+
+    public void digito4(ActionEvent actionEvent) {
+    }
+
+    public void digito5(ActionEvent actionEvent) {
+    }
+
+    public void digito6(ActionEvent actionEvent) {
+    }
+
+    public void digito7(ActionEvent actionEvent) {
+    }
+
+    public void digito8(ActionEvent actionEvent) {
+    }
+
+    public void digito9(ActionEvent actionEvent) {
+    }
+
+    public void corrige(ActionEvent actionEvent) {
+    }
+
+    public void votarBranco(ActionEvent actionEvent) {
+    }
+
+    public void confirmarVoto(ActionEvent actionEvent) {
+    }
+}
