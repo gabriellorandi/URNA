0873dcce15778670fe28370a75bb8a0221182de9
Brendow Silva <brendow.silva@vistajet.com>
2020-08-30 20:43:32 -0300

    Implemented PDFUtils class

--- src/Utils/ValidateFields.java
+++ src/Utils/ValidateFields.java
@@ -7,8 +7,8 @@ public class ValidateFields {
     private static final String REGEX_NUMBER;
 
     static {
-        REGEX_TEXT = "[a-zA-Z]";
-        REGEX_NUMBER = "[0-9]";
+        REGEX_TEXT = "[a-zA-Z\\u00C0-\\u00FF ]*";
+        REGEX_NUMBER = "[0-9]*";
     }
 
     public static boolean validateTextField (String content) {
